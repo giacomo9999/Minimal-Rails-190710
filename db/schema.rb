f2909153ec6a9025dfe1ba61a2e64cd07b841d5d
James Gary <gmarchesi888@gmail.com>
2019-07-10 19:10:57 -0400

    Revised schema and seeded DB.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_224350) do
+ActiveRecord::Schema.define(version: 2019_07_10_230005) do
 
   create_table "fruits", force: :cascade do |t|
     t.string "color"
     t.string "shape"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Fruit.create(name: "Banana", color: "yellow", shape: "cylinder")
+Fruit.create(name: "Orange", color: "orange", shape: "spheroid")
+Fruit.create(name: "Apple", color: "red", shape: "spheroid")
+Fruit.create(name: "Lemon", color: "yellow", shape: "irregular")
+Fruit.create(name: "Plum", color: "purple", shape: "spheroid")
+Fruit.create(name: "Pear", color: "green", shape: "irregular")
