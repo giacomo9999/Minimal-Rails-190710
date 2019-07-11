14bbe3985230b4b6a227d8a5d4b3ac6439cfa4d9
James Gary <gmarchesi888@gmail.com>
2019-07-10 21:56:00 -0400

    Added 'Create' functionality.

--- app/controllers/fruits_controller.rb
+++ app/controllers/fruits_controller.rb
@@ -4,11 +4,31 @@ class FruitsController < ApplicationController
   end
 
   def show
+    @fruit = Fruit.find(params[:id])
   end
 
   def new
+    @fruit = Fruit.new
+  end
+
+  def create
+    @fruit = Fruit.create(fruit_params)
+    redirect_to fruits_path
   end
 
   def edit
+    @fruit = Fruit.find(params[:id])
+  end
+
+  def update
+    @fruit = Fruit.find(params[:id])
+    @fruit.update(fruit_params)
+    redirect_to fruits_path
+  end
+
+  private
+
+  def fruit_params
+    params.require(:fruit).permit(:name, :shape, :color)
   end
 end
